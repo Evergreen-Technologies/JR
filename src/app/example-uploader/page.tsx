e311189beeadbf19632810dcf5faa1c1e8c7214b
awoliop <awoliioop@gmail.com>
2024-11-03 15:11:20 +0300

    submit button minor adjustements

--- src/app/example-uploader/page.tsx
+++ src/app/example-uploader/page.tsx
@@ -153,7 +153,7 @@ export default function Home() {
         <button
           type="submit"
           onClick={handleSubmit}
-          className="border border-white bg-[#0a660a] text-black font-bold disabled:bg-white disabled:text-gray-500 rounded-md h-10"
+          className="border border-white bg-[#0a660a] text-black font-bold disabled:bg-white disabled:text-gray-500 rounded-md h-10 border-none"
           disabled={!save}
         >
           Submit
