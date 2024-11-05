67133ab471f317a5cb5745c512ef8284bb4c59ee
awoliop <awoliioop@gmail.com>
2024-11-05 17:42:38 +0300

    feat(JR-101): Embed a video player into the video library for users

--- src/app/user/video_library/page.tsx
+++ src/app/user/video_library/page.tsx
@@ -4,18 +4,17 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import React from "react";
 import ReactPlayer from "react-player/lazy";
+import Video from "next-video";
+import Plyr from "plyr-react";
+import "plyr/dist/plyr.css";
+
+import { Progress } from "@/components/ui/progress";
+
 // import { CldVideoPlayer } from "next-cloudinary";
 // import "next-cloudinary/dist/cld-video-player.css";
 
 const Page = () => {
-  const [render, setRender] = useState([
-    {
-      title: "video Title",
-      imageUrl:
-        "https://imgs.search.brave.com/oEmjW0UDCCn7R5L6OlJ77LHvDU8sIP2KycVcV4w1uw4/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLmV0/c3lzdGF0aWMuY29t/LzEwNTI1ODE4L3Iv/aWwvZTg5ZTNhLzM4/NzA4OTQzNjMvaWxf/NjAweDYwMC4zODcw/ODk0MzYzXzMxcjUu/anBn",
-      videoUrl: "https://youtu.be/SqcY0GlETPk",
-    },
-  ]);
+  const [render, setRender] = useState([]);
   const render_video_library = async () => {
     try {
       const re1 = await axios.get("/api/Materials");
@@ -30,21 +29,52 @@ const Page = () => {
   useEffect(() => {
     render_video_library();
   }, []);
+
+  const [progress, setProgress] = React.useState(13);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 100);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="lg:container lg:w-full lg:mx-auto mx-[20px] min-h-[93vh]  mt-10 rounded-[30px] shadow-2xl">
       <div className="flex flex-wrap gap-10 justify-center items-center w-full p-10">
-        <div className="w-[400px] h-[400px]  rounded-[12px] flex items-center justify-center shadow-xl relative">
-          {render.map((vid, index) => (
-            <div className="absolute bottom-0 h-24 transition-all ease-in-out bg-gray-300 text-black opacity-100 w-full flex items-center justify-center rounded-b-[12px] ">
-              Title
+        {!render[0] && <Progress value={progress} className="w-[10%] " />}
+
+        {render.map((vid: any, index: number) => (
+          <div className="w-[400px] h-[380px]  rounded-[12px] flex items-center justify-center shadow-xl relative">
+            <Plyr
+              source={{
+                type: "video",
+                sources: [
+                  {
+                    src: vid.videoUrl,
+                    type: "video/mp4",
+                  },
+                ],
+              }}
+              options={{
+                controls: [
+                  "play-large",
+                  "play",
+                  "progress",
+                  "current-time",
+                  "mute",
+                  "volume",
+                  "fullscreen",
+                ],
+              }}
+            />
+            <div className="absolute bottom-0 h-20 transition-all ease-in-out bg-gray-300 text-black opacity-100 w-full flex items-center justify-center rounded-b-[12px] ">
+              {vid.title}
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
 export default Page;
-
 // <CldVideoPlayer width="1920" height="1080" src={vid.videoUrl} />
